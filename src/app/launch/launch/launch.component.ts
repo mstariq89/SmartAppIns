37be8dddaa5cae9b19720eaf92b02f39f9f4f02a
Mohanraj <mvh5cob@rbeigcn.com>
2019-09-11 22:58:33 +0530

    test

--- src/app/launch/launch/launch.component.ts
+++ src/app/launch/launch/launch.component.ts
@@ -10,6 +10,21 @@ export class LaunchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const clientSettings: FHIR.SMART.OAuth2ClientSettings = {
+      client_id: '7a20c455-b146-403b-a7e5-86ab4846259a',
+      // Adding the scopes launch openid and profile
+      scope: 'patient/Patient.read patient/Observation.read launch online_access openid profile patient/MedicationOrder.read',
+    };
+  
+    const oauth2Configuration: FHIR.SMART.OAuth2Configuration = {
+      client: clientSettings,
+    };
+  
+    FHIR.oauth2.authorize(oauth2Configuration, (authError) => {
+      console.log('Error in Authorize');
+    });
   }
 
 }
+
