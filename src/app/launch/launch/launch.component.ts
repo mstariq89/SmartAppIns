843de3569104efb760941923aa6163d627c1b43f
Mohanraj Velusamy <mvh5cob@rbeigcn.com>
2019-09-12 15:20:40 +0530

    test

--- src/app/launch/launch/launch.component.ts
+++ src/app/launch/launch/launch.component.ts
@@ -16,6 +16,7 @@ export class LaunchComponent implements OnInit {
       client_id: '7a20c455-b146-403b-a7e5-86ab4846259a',
       // Adding the scopes launch openid and profile
       scope: 'patient/Patient.read patient/Observation.read launch online_access openid profile patient/MedicationOrder.read',
+      redirect_uri: 'https://vmr1689.github.io/SmartAppIns/test/test1'
     };
   
     const oauth2Configuration: FHIR.SMART.OAuth2Configuration = {
